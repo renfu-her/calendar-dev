396ceb1f67fe8e8562997750feda10f860dc8e4f
Patrick Her <renfu.her@gmail.com>
2023-09-01 21:57:00 +0800

    update route

---
+++ app/Http/Controllers/Api/LoginAdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginAdminController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        $user = auth('api')->user();
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\CalendarApiController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\LoginAdminController;
 
 route::get('/calendar/member/{id}', [CalendarApiController::class, 'calendarMember']);
 route::post('/calendar/events', [CalendarApiController::class, 'calendarEvent']);
 route::delete('/calendar/delete', [CalendarApiController::class, 'calendarEventDelete']);
 route::post('/calendar/update', [CalendarApiController::class, 'calendarEventUpdate']);
+
+route::get('/login', [LoginAdminController::class, 'login']);
+route::group(['middleware' => 'auth:api'], function () {
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'user']);
+});
